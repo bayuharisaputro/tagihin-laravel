6455b196110c683d14f4aa932be62017c94c2ca5
TANGGON <TANGGON>
2018-08-10 11:23:47 +0700

    updet waktu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,35 +24,35 @@
         <th>Nomor Hp Penagih</th>
         <th>Jumlah Tagihan</th>
         <th>Catatan</th>
-        <th>Tanggal2324</th>
+        <th>Tanggal</th>
       </tr>
     </thead>
     <?php 
+    
    // print_r($bios["rows"]);
- //   echo ($bios["rows"][$count]["value"][$counter]);
+//   echo ($bios["rows"][$count]["value"][$counter]);
 // echo date('Y-m-d',1533885240-strtotime($bios["rows"][$count]["value"][8]))
 // echo 1533945600-strtotime($bios["rows"][$count]["value"][8])
-// 
+//    strtotime(date('Y-m-d H:i:s'))-strtotime($bios["rows"][$count]["value"][8]);
     ?>
     <tbody>
       
       <?php 
-      for($count = 0 ;$count <count($bios["rows"]); $count ++ ){
-      echo "<tr>";
-      for($counter = 0 ;$counter <count($bios["rows"][0]["value"]);$counter++ ) { ?>
-        <td><?php 
-    echo ($bios["rows"][$count]["value"][$counter]);
-    ?></td>
-      <?php }
+      date_default_timezone_set("Asia/Jakarta");
+      for($count = 0 ;$count <count($bios["rows"]); $count ++ ){    
+        echo "<tr>";
+        for($counter = 0 ;$counter <count($bios["rows"][0]["value"]);$counter++ ) {
+      ?>
+          <td>
+          <?php
+            echo ($bios["rows"][$count]["value"][$counter]);
+          ?>
+          </td>
+        <?php
+        }
         echo "</tr>";
-    } ?>
-        
-
-       
-        
-      
-      
-  
+      }
+      ?>
     </tbody>
   </table>
   </div>
